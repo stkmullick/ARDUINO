efbc5e27a398d3ad6ac289f951643a9a03869a86
stkmullick <90825875+stkmullick@users.noreply.github.com>
2021-09-16 13:04:52 +0530

    Add files via upload

---
+++ traffic_light.ino
@@ -0,0 +1,36 @@
+const int red=8;
+const int green=13;
+void setup() {
+  pinMode(red,OUTPUT);
+  pinMode(green,OUTPUT);
+  }
+
+void loop() {
+  digitalWrite(red,HIGH);
+  delay(4000);
+  digitalWrite(red,LOW);
+  delay(500);
+  digitalWrite(red,HIGH);
+  delay(500);
+  digitalWrite(red,LOW);
+  delay(500);
+  digitalWrite(red,HIGH);
+  delay(500);
+  digitalWrite(red,LOW);
+  delay(500);
+  digitalWrite(red,HIGH);
+  delay(500);
+  digitalWrite(red,LOW);
+  delay(500);
+  digitalWrite(red,HIGH);
+  delay(500);
+  digitalWrite(red,LOW);
+  delay(500);
+  digitalWrite(green,HIGH);
+  delay(4000);
+ digitalWrite(green ,LOW);
+  delay(1000);
+  
+  // put your main code here, to run repeatedly:
+
+}
