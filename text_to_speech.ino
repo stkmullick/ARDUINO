eca966484143400646e3255f3f00a4c6329560eb
stkmullick <90825875+stkmullick@users.noreply.github.com>
2021-09-16 13:02:03 +0530

    Update text_to_speech.ino

--- text_to_speech.ino
+++ text_to_speech.ino
@@ -8,11 +8,15 @@ void setup() {
 }
 
 void loop() {
-   voice.say(spPAUSE2);
-   voice.say(sp2_MY);
-   voice.say(sp2_DANGER);
-   voice.say(sp3_STORM);
-   voice.say(sp3_IN);
-   voice.say(sp3_THE);// put your main code here, to run repeatedly:
+  voice.say(sp2_U);
+  voice.say(sp2_R);
+  voice.say(sp4_MUCH);
+  voice.say(sp4_NEAR);
+  voice.say(sp3_TWO);
+  voice.say(sp2_ON);
+  voice.say(sp2_C);
+  voice.say(sp2_Y);
+  voice.say(sp3_IS);
+  voice.say(sp4_GOOD);
 
 }
